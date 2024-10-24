f38f50fd134b27ffbf270bbdfaefb3c3cec0d654
LouisNdt <louis.ndt31@gmail.com>
2024-10-24 23:27:57 +0200

    chore: WIP animations

--- lib/model/PlayerModel.dart
+++ lib/model/PlayerModel.dart
@@ -1,13 +1,16 @@
 import 'package:blackjack/model/CardModel.dart';
 import 'package:blackjack/model/DeckModel.dart';
+import 'package:flutter/animation.dart';
 
 class PlayerModel {
   List<CardModel> playerCards = [];
   int points = 0;
 
-  void drawCardForPlayer(DeckModel deck) {
+  void drawCardForPlayer(DeckModel deck, AnimationController animationController) {
     CardModel cardModel = deck.drawCard();
     playerCards.add(cardModel);
     points += cardModel.points;
+    animationController.reset();
+    animationController.forward();
   }
 }
